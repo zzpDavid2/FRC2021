42c2942cd9c63d0289ba96cb9f97e76194e25276
zzpdavid2 <zzpdavid2L@gmail.com>
2021-09-22 22:52:17 -0400

    Initial commit

---
+++ Code/src/main/java/frc/robot/Drive.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+Public Class driveControl(){
+    public void diff_drive(){
+        
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FRC2021
+ David Zhou's code for FRC 2021 season.
